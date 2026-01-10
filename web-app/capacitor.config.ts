40e38dda99312814bd55fb41b34531ce392f551f
Pixelplanet <tom.winnerl@web.de>
2026-01-10 11:41:19 +0100

    Enable CapacitorHttp to fix network issues

--- web-app/capacitor.config.ts
+++ web-app/capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.filamentdb',
   appName: 'FilamentDB',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    androidScheme: 'http',
+    allowNavigation: ['*'],
+    cleartext: true
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
